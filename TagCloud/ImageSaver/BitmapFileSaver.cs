1f5810c3b340f23b5fa35cca61116326b1d32c31
Geratoptus <shura.bocharov.2004@mail.ru>
2025-01-02 16:12:08 +0500

    feat(ImageSaver): переписал сейвера на Result

--- TagCloud/ImageSaver/BitmapFileSaver.cs
+++ TagCloud/ImageSaver/BitmapFileSaver.cs
@@ -1,24 +1,26 @@
 using System.Drawing;
+using ResultTools;
 
 namespace TagCloud.ImageSaver;
 
 #pragma warning disable CA1416
 public class BitmapFileSaver(string imageName, string imageFormat) : IImageSaver
 {
-    private readonly List<string> _supportedFormats = ["png", "jpg", "jpeg", "bmp"];
+    private readonly List<string> supportedFormats = ["png", "jpg", "jpeg", "bmp"];
 
     public BitmapFileSaver(FileSaveSettings settings)
         : this(settings.ImageName, settings.ImageFormat)
     { }
 
-    public string Save(Bitmap image)
-    {
-        if (!_supportedFormats.Contains(imageFormat))
-            throw new ArgumentException($"Unsupported image format: {imageFormat}");
+    public Result<string> Save(Bitmap image) 
+        => !supportedFormats.Contains(imageFormat) 
+            ? Result.Fail<string>($"Unsupported image format: {imageFormat}")
+            : $"{imageName}.{imageFormat}".AsResult().Then(n => SaveImage(image, n));
 
-        var fullImageName = $"{imageName}.{imageFormat}";
-        image.Save(fullImageName);
-        return Path.Combine(Directory.GetCurrentDirectory(), fullImageName);
+    private static string SaveImage(Bitmap image, string name)
+    {
+        image.Save(name);
+        return Path.Combine(Directory.GetCurrentDirectory(), name);
     }
 }
 #pragma warning restore CA1416
--- TagCloud/ImageSaver/IImageSaver.cs
+++ TagCloud/ImageSaver/IImageSaver.cs
@@ -1,8 +1,9 @@
 using System.Drawing;
+using ResultTools;
 
 namespace TagCloud.ImageSaver;
 
 public interface IImageSaver
 {
-    public string Save(Bitmap image);
+    public Result<string> Save(Bitmap image);
 }
